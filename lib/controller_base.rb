b0932e15bace67c560c211cb370e52cca9ad7edf
jilldela <jilldelosangeles@gmail.com>
2017-07-18 17:53:03 -0700

    Create Router class

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,9 +8,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params = {})
     @req = req
     @res = res
+    @params = params.merge(@req.params)
     @already_built_response = false
   end
 
@@ -64,6 +65,8 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
 
--- lib/router.rb
+++ lib/router.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
@@ -16,6 +18,13 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    matched = @pattern.match(req.path) #MatchData object returned by Regexp#match
+
+    route_params = Hash[matched.names.zip(matched.captures)]
+
+    @controller_class
+      .new(req, res, route_params)
+      .invoke_action(@action_name)
   end
 end
 
@@ -23,27 +32,47 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    new_route = Route.new(pattern, method, controller_class, action_name)
+    @routes << new_route
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class,action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
+    nil
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    @routes.each do |route|
+      matched = match(req)
+      if matched
+        matched.run(req, res)
+      else
+        res.status = 404
+        res.write("Route not found")
+      end
+    end
   end
 end
